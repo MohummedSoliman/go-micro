55bf8435fa141aad7ec02951343893157d2f7653
Mohamed Ibranim <mosuliman92@gmail.com>
2025-10-05 16:41:45 +0300

    create method for conneting to db

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
 	"authentication/data"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = "80"
+
+var count int
 
 type Config struct {
 	DB     *sql.DB
@@ -14,4 +25,63 @@ type Config struct {
 }
 
 func main() {
+	log.Println("Starting Authentication Service")
+
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't Connect To PostgreSQL")
+	}
+
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres Not Yet Ready!!!")
+			count++
+		} else {
+			log.Println("Connected To PostgreSQL")
+			return connection
+		}
+
+		if count > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing Off for Two Seconds")
+		time.Sleep(2 * time.Second)
+		continue
+	}
 }
